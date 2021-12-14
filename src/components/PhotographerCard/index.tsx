9f320a460ba8e68d8df23aa079d8e6c537df8d85
Laurent Hirth <laurent.hirth@protonmail.com>
2021-12-14 16:39:03 +0100

    add mui

--- src/components/PhotographerCard/index.tsx
+++ src/components/PhotographerCard/index.tsx
@@ -1,3 +1,10 @@
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from '@mui/material'
 import { Link } from 'react-router-dom'
 import Tag from '../Tag'
 import {
@@ -32,23 +39,43 @@ export default function PhotographerCard({
   portrait,
 }: PhotographerCardProps) {
   return (
-    <StyledPhotographerCard>
-      <Link to={`/profile/${id}`}>
-        <StyledPortrait>
-          <img src={portrait} alt="" />
-        </StyledPortrait>
-        <StyledName>{name}</StyledName>
-      </Link>
-      <StyledCity>
-        {city}, {country}
-      </StyledCity>
-      <StyledTagline>{tagline}</StyledTagline>
-      <StyledPrice>{price}</StyledPrice>
-      <StyledTagContainer>
-        {tags.map((tag) => (
-          <Tag key={tag} tag={tag} />
-        ))}
-      </StyledTagContainer>
-    </StyledPhotographerCard>
+    <Card sx={{ width: 300, height: 300 }}>
+      <CardActionArea href={`/profile/${id}`}>
+        <CardMedia
+          component="img"
+          image={portrait}
+          alt={'portrait de ' + name}
+          width={300}
+          height={200}
+          object-fit={'cover'}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {tagline}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    // <StyledPhotographerCard>
+    //   <Link to={`/profile/${id}`}>
+    //     <StyledPortrait>
+    //       <img src={portrait} alt="" />
+    //     </StyledPortrait>
+    //     <StyledName>{name}</StyledName>
+    //   </Link>
+    //   <StyledCity>
+    //     {city}, {country}
+    //   </StyledCity>
+    //   <StyledTagline>{tagline}</StyledTagline>
+    //   <StyledPrice>{price}</StyledPrice>
+    //   <StyledTagContainer>
+    //     {tags.map((tag) => (
+    //       <Tag key={tag} tag={tag} />
+    //     ))}
+    //   </StyledTagContainer>
+    // </StyledPhotographerCard>
   )
 }
