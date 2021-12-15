31e39dd4286a8e25a962c0d4c9467755b9209ab3
Laurent Hirth <laurent.hirth@protonmail.com>
2021-12-15 08:39:51 +0100

    remove mui

--- src/components/MediaCard/index.tsx
+++ src/components/MediaCard/index.tsx
@@ -1,10 +1,3 @@
-import {
-  Card,
-  CardActionArea,
-  CardContent,
-  CardMedia,
-  Typography,
-} from '@mui/material'
 import { StyledMediaCard } from './style'
 
 interface MediaCardProps {
@@ -25,63 +18,36 @@ export default function MediaCard({
   const isVideo = filename?.split('.').pop() === 'mp4'
 
   return (
-    <Card>
-      <CardActionArea>
+    <StyledMediaCard>
+      <div className="media">
         {isVideo ? (
-          <CardMedia
-            component="video"
-            height="250"
-            src={`http://${process.env.REACT_APP_API}:${process.env.REACT_APP_PORT}${process.env.REACT_APP_PATH_TO_MEDIA_SMALL}${filename}`}
-          />
+          <video controls loop>
+            <source
+              src={`http://${process.env.REACT_APP_API}:${process.env.REACT_APP_PORT}${process.env.REACT_APP_PATH_TO_MEDIA_SMALL}${filename}`}
+              type="video/mp4"
+            />
+          </video>
         ) : (
-          <CardMedia
-            component="img"
-            height="250"
-            image={`http://${process.env.REACT_APP_API}:${process.env.REACT_APP_PORT}${process.env.REACT_APP_PATH_TO_MEDIA_SMALL}${filename}`}
-            alt="green iguana"
-          />
+          <picture>
+            <source
+              srcSet={`http://${process.env.REACT_APP_API}:${process.env.REACT_APP_PORT}${process.env.REACT_APP_PATH_TO_MEDIA_MEDIUM}${filename}`}
+              media="(min-width: 768px)"
+            />
+            <img
+              src={`http://${process.env.REACT_APP_API}:${process.env.REACT_APP_PORT}${process.env.REACT_APP_PATH_TO_MEDIA_SMALL}${filename}`}
+              alt={alt}
+            />
+          </picture>
         )}
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {title}
-          </Typography>
-          <Typography gutterBottom variant="h6" component="div">
-            {price}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-
-    // <StyledMediaCard>
-    //   <div className="media">
-    //     {isVideo ? (
-    //       <video controls loop>
-    //         <source
-    //           src={`http://${process.env.REACT_APP_API}:${process.env.REACT_APP_PORT}${process.env.REACT_APP_PATH_TO_MEDIA_SMALL}${filename}`}
-    //           type="video/mp4"
-    //         />
-    //       </video>
-    //     ) : (
-    //       <picture>
-    //         <source
-    //           srcSet={`http://${process.env.REACT_APP_API}:${process.env.REACT_APP_PORT}${process.env.REACT_APP_PATH_TO_MEDIA_MEDIUM}${filename}`}
-    //           media="(min-width: 768px)"
-    //         />
-    //         <img
-    //           src={`http://${process.env.REACT_APP_API}:${process.env.REACT_APP_PORT}${process.env.REACT_APP_PATH_TO_MEDIA_SMALL}${filename}`}
-    //           alt={alt}
-    //         />
-    //       </picture>
-    //     )}
-    //   </div>
-    //   <div className="textContainer">
-    //     <div className="title">{title}</div>
-    //     <div className="price">{price}€</div>
-    //     <div className="likes">
-    //       {likes}
-    //       {'<3'}
-    //     </div>
-    //   </div>
-    // </StyledMediaCard>
+      </div>
+      <div className="textContainer">
+        <div className="title">{title}</div>
+        <div className="price">{price}€</div>
+        <div className="likes">
+          {likes}
+          {'<3'}
+        </div>
+      </div>
+    </StyledMediaCard>
   )
 }
--- src/components/PhotographerCard/index.tsx
+++ src/components/PhotographerCard/index.tsx
@@ -1,11 +1,4 @@
-import {
-  Card,
-  CardActionArea,
-  CardContent,
-  CardMedia,
-  Typography,
-} from '@mui/material'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import {
   StyledCity,
   StyledName,
@@ -35,38 +28,18 @@ export default function PhotographerCard({
   portrait,
 }: PhotographerCardProps) {
   return (
-    <Card sx={{ width: 300 }}>
-      <CardActionArea component={Link} to={`/profile/${id}`}>
-        <CardMedia
-          component="img"
-          image={portrait}
-          alt={'portrait de ' + name}
-          width={300}
-          height={200}
-          object-fit={'cover'}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {name}
-          </Typography>
-          <Typography variant="h6" color="text.secondary">
-            {tagline}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-    // <StyledPhotographerCard>
-    //   <Link to={`/profile/${id}`}>
-    //     <StyledPortrait>
-    //       <img src={portrait} alt="" />
-    //     </StyledPortrait>
-    //     <StyledName>{name}</StyledName>
-    //   </Link>
-    //   <StyledCity>
-    //     {city}, {country}
-    //   </StyledCity>
-    //   <StyledTagline>{tagline}</StyledTagline>
-    //   <StyledPrice>{price}</StyledPrice>
-    // </StyledPhotographerCard>
+    <StyledPhotographerCard>
+      <Link to={`/profile/${id}`}>
+        <StyledPortrait>
+          <img src={portrait} alt="" />
+        </StyledPortrait>
+        <StyledName>{name}</StyledName>
+      </Link>
+      <StyledCity>
+        {city}, {country}
+      </StyledCity>
+      <StyledTagline>{tagline}</StyledTagline>
+      <StyledPrice>{price}</StyledPrice>
+    </StyledPhotographerCard>
   )
 }
