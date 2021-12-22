bebaffd410c0cdf2c282abec3d2bbf26820e03d1
Laurent Hirth <laurent.hirth@protonmail.com>
2021-12-22 18:57:49 +0100

    photo wip

--- src/components/MediaCard/index.tsx
+++ src/components/MediaCard/index.tsx
@@ -1,7 +1,9 @@
 import { device } from '../../utils/style/responsive'
 import { StyledHider, StyledMediaCard, StyledSlidingBanner } from './style'
-import { useState } from 'react'
+import { Suspense, useEffect, useRef, useState } from 'react'
 import { useImage } from 'react-image'
+import Spinner from '../Spinner'
+import { ContentCopy } from '@mui/icons-material'
 
 interface MediaCardProps {
   title: string
@@ -12,6 +14,95 @@ interface MediaCardProps {
   alt: string
 }
 
+const usePrevious = <T extends any>(value: T) => {
+  const ref = useRef<T>()
+  useEffect(() => {
+    ref.current = value
+  }, [value])
+  return ref.current
+}
+const useRequestAnimationFrame = (): [(cb: () => void) => void, Function] => {
+  const handles = useRef<number[]>([])
+  const _raf = (cb: () => void) => {
+    handles.current.push(requestAnimationFrame(cb))
+  }
+  const _resetRaf = () => {
+    handles.current.forEach((id) => cancelAnimationFrame(id))
+    handles.current = []
+  }
+
+  return [_raf, _resetRaf]
+}
+
+type ImageProps = {
+  src: string
+  alt?: string
+  transitionDuration?: number
+  curve?: string
+}
+
+const CrossFadeImage = (props: ImageProps) => {
+  const { src, alt, transitionDuration = 0.35, curve = 'ease' } = props
+  const oldSrc = usePrevious(src)
+  const [topSrc, setTopSrc] = useState<string>(src)
+  const [bottomSrc, setBottomSrc] = useState<string>('')
+  const [bottomOpacity, setBottomOpacity] = useState(0)
+  const [display, setDisplay] = useState(false)
+  const [raf, resetRaf] = useRequestAnimationFrame()
+
+  useEffect(() => {
+    if (src !== oldSrc) {
+      resetRaf()
+      setTopSrc('')
+      setBottomSrc('')
+
+      raf(() => {
+        setTopSrc(src)
+        setBottomSrc(oldSrc!)
+        setBottomOpacity(99)
+
+        raf(() => {
+          setBottomOpacity(0)
+        })
+      })
+    }
+  }, [])
+
+  return (
+    <div
+      className="imgContainer"
+      // style={{
+      //   position: 'relative',
+      //   height: '100%',
+      // }}
+    >
+      {topSrc && (
+        <img
+          style={{
+            // position: 'absolute',
+            opacity: display ? '100%' : 0,
+            transition: `opacity ${transitionDuration}s ${curve}`,
+          }}
+          onLoad={() => setDisplay(true)}
+          src={topSrc}
+          alt={alt}
+        />
+      )}
+      {bottomSrc && (
+        <img
+          style={{
+            // position: 'absolute',
+            opacity: bottomOpacity + '%',
+            transition: `opacity ${transitionDuration}s ${curve}`,
+          }}
+          src={bottomSrc}
+          alt={alt}
+        />
+      )}
+    </div>
+  )
+}
+
 export default function MediaCard({
   title,
   filename,
@@ -20,7 +111,7 @@ export default function MediaCard({
   likes,
   alt,
 }: MediaCardProps) {
-  const [status, setStatus] = useState('loading')
+  const [loaded, setLoaded] = useState(false)
   const isVideo = type === 'video'
 
   const numberWithSpaces = (x: number) =>
@@ -28,46 +119,45 @@ export default function MediaCard({
 
   const handlePictureLoaded = (e: any) => {
     console.log('loaded: ', e)
-    setStatus('loaded')
-  }
-
-  function Image() {
-    const { src, isLoading } = useImage({
-      srcList: `http://${process.env.REACT_APP_API}:${process.env.REACT_APP_PORT}${process.env.REACT_APP_PATH_TO_MEDIA_MEDIUM}${filename}`,
-    })
-    return <img src={src} alt="" />
+    setLoaded(true)
   }
 
   return (
     <>
       {/* {status === 'loading' ?? <StyledHider>{status}</StyledHider>} */}
       <StyledMediaCard>
-        {!isVideo && <Image />}
-        {/* {isVideo ? (
-          <video loop autoPlay>
-            <source
-              src={`http://${process.env.REACT_APP_API}:${process.env.REACT_APP_PORT}${process.env.REACT_APP_PATH_TO_MEDIA_SMALL}${filename}`}
-              type="video/mp4"
-            />
-          </video>
-        ) : (
-          <picture>
-            <source
-              srcSet={`http://${process.env.REACT_APP_API}:${process.env.REACT_APP_PORT}${process.env.REACT_APP_PATH_TO_MEDIA_MEDIUM}${filename}`}
-              media={device.smallDesktop}
-              onLoad={(e) => handlePictureLoaded(e)}
-            />
-            <img
-              src={`http://${process.env.REACT_APP_API}:${process.env.REACT_APP_PORT}${process.env.REACT_APP_PATH_TO_MEDIA_SMALL}${filename}`}
-              alt={alt}
-              onLoad={(e) => handlePictureLoaded(e)}
-            />
-          </picture>
-        )} */}
-        <StyledSlidingBanner>
+        {isVideo ? null : (
+          // <video loop autoPlay>
+          //   <source
+          //     src={`http://${process.env.REACT_APP_API}:${process.env.REACT_APP_PORT}${process.env.REACT_APP_PATH_TO_MEDIA_SMALL}${filename}`}
+          //     type="video/mp4"
+          //   />
+          // </video>
+          // <picture>
+          //   <source
+          //     srcSet={`http://${process.env.REACT_APP_API}:${process.env.REACT_APP_PORT}${process.env.REACT_APP_PATH_TO_MEDIA_MEDIUM}${filename}`}
+          //     media={device.smallDesktop}
+          //     onLoad={(e) => handlePictureLoaded(e)}
+          //   />
+          //   <img
+          //     src={`http://${process.env.REACT_APP_API}:${process.env.REACT_APP_PORT}${process.env.REACT_APP_PATH_TO_MEDIA_MEDIUM}${filename}`}
+          //     alt={alt}
+          //   />
+          // </picture>
+          // <Suspense fallback={<Spinner />}>
+
+          <CrossFadeImage
+            src={`http://${process.env.REACT_APP_API}:${process.env.REACT_APP_PORT}${process.env.REACT_APP_PATH_TO_MEDIA_MEDIUM}${filename}`}
+            // onLoad={(e) => handlePictureLoaded(e)}
+            alt=""
+          />
+
+          // </Suspense>
+        )}
+        {/* <StyledSlidingBanner>
           <div>{title}</div>
           <div>{numberWithSpaces(price)}€</div>
-        </StyledSlidingBanner>
+        </StyledSlidingBanner> */}
       </StyledMediaCard>
     </>
   )
